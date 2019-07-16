b8daa5865d091cc90a56cc64ddbb5235604ce7cb
Akshay kumar Sharma <aksss24705@gmail.com>
2019-07-17 00:08:08 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+import 'package:projects/ui/climatic.dart';
+//import 'package:climate/ui/climatic.dart';
+
+void main()
+{
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "climate",
+    home: Klimatic(),
+  ));
+}
---
+++ lib/util/utils.dart
@@ -0,0 +1,2 @@
+final appId = "1f06a72abec67e6551ed4ba1d11d8f32";
+final defaultCity = "Jaipur, Rajasthan, India";
